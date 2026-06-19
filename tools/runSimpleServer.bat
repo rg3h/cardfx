7d58d8438ca32848c992fe53dcb86f02254e18cb
rg3h <rich.gossweiler@gmail.com>
2026-06-19 08:42:13 -0400

    initial update

---
+++ tools/runSimpleServer.bat
@@ -0,0 +1,13 @@
+:: @fileoverview runSimpleServer.bat runs a local node web server for public/
+@echo off
+clear
+setlocal
+color 1F
+
+:: pushd works for UNC paths (\\server\); %~d0 is drive and %~p0 is code's dir
+pushd %~d0%~p0simpleServer
+pwd
+set NO_COLOR=1
+cmd /c node . --webroot ../../public
+
+endlocal
